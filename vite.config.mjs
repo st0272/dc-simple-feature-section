d0d6f2dbe915868cda80f150a6a8fff976793a58
Suzuka Takeuchi <sztk1@nifty.com>
2025-04-20 14:06:44 +0900

    Modify files

--- vite.config.mjs
+++ vite.config.mjs
@@ -2,9 +2,9 @@ import { defineConfig } from "vite";
 import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
-  base: "/",
-  server: {
-    open: true,
-  },
+  base: "/dc-simple-feature-section/",
+  // server: {
+  //   open: true,
+  // },
   plugins: [tailwindcss()],
 });
